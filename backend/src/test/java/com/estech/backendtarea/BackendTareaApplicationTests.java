b47799fc1f15588c99b7fd1ed0cdf3d7be672f1b
Enrique <enrikus2004@gmail.com>
2024-02-06 18:06:05 +0100

    initial backend

---
+++ backend/src/test/java/com/estech/backendtarea/BackendTareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estech.backendtarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
